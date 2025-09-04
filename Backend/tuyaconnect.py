6951891d4430ccbf56a650d084c126a909a95681
jiro <jiro_rafael_layug@dlsl.edu.ph>
2025-09-04 18:09:10 +0800

    final commits for this module

--- Backend/tuyaconnect.py
+++ Backend/tuyaconnect.py
@@ -240,7 +240,7 @@ while not stop_flag:
                 continue
 
             # kWh calculation (5 min)
-            energy_kwh = (power_watts / 1000.0) * (5.0 / 60.0) #3.0 / 60.0 if 3 minutes
+            energy_kwh = (power_watts / 1000.0) * (1.0 / 60.0) #3.0 / 60.0 if 3 minutes
 
             # Active/inactive handling
             if power_watts == 0:
@@ -292,7 +292,7 @@ while not stop_flag:
         print("Error occurred:", e)
 
     # Sleep loop
-    for _ in range(300): #change to 180 if 3 minutes, 300 if 5 minutes
+    for _ in range(60): #change to 180 if 3 minutes, 300 if 5 minutes
         if stop_flag:
             break
         time.sleep(1)
